2b3e27fca10618f9dfa0d96cd0a69bec98518168
Quasar Wei <quasar.wei@outlook.com>
2019-08-23 09:24:15 -0700

    wrote api createItem and rewrote store add item

--- scripts/api.js
+++ scripts/api.js
@@ -2,12 +2,26 @@
 
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/phoebe-quasar';
+
   function getItems() {
-    return fetch (`${BASE_URL}/items`)
+    return fetch (`${BASE_URL}/items`);
     // Promise.resolve('A successful response!'); 
   }
 
+  const createItem = function(name) {
+    let newItem = {
+      name
+    };
+
+    newItem = JSON.stringify(newItem);
+
+    return fetch(`${BASE_URL}/items`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: newItem});
+
+  };
+
   return {
-    getItems
-  }
+    getItems,
+    createItem
+  };
+
 })();
